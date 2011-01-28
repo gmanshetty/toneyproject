48b986f8af125a68aebd0663f0b688493feeb3ea
Gmanshetty <gmanshetty@ymail.com>
2011-01-28 02:16:37 -0500

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toneyproject::Application.config.secret_token = '68c4e26d58c4cabc5f0274974d0f5e59002beaa490ddf97f39d8054912d857453752bee6d71c6870f1c56d777520ddd61d747a5f56b40a88a2fb6c177d770eae'
